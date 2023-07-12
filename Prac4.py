fea9bdd00f61f095243afa1910c73b2fd9b1b08e
Heet Thummar <thummarhit6271@gmail.com>
2023-07-12 10:09:12 +0530

    P4

---
+++ Prac4.py
@@ -0,0 +1,8 @@
+a=input('Enter value of A:')
+b=input('Enter value of B:')
+temp=a
+a=b
+b=temp
+print('The value of a after swapping:{}'.format(a))
+print('The value of b after swapping:{}'.format(b))
+
